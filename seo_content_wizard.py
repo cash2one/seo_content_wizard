166d7f725b2e48b6d6c52dc16e9d7401c02badf2
slavarybalka <slavarybalko@gmail.com>
2016-05-31 16:46:57 -0500

    updted

--- seo_content_wizard.py
+++ seo_content_wizard.py
@@ -25,12 +25,13 @@ from collections import defaultdict
 
 
 
+
 if sys.stdout.encoding != 'utf-8':
   sys.stdout = codecs.getwriter('utf-8')(sys.stdout.buffer, 'strict')
 if sys.stderr.encoding != 'utf-8':
   sys.stderr = codecs.getwriter('utf-8')(sys.stderr.buffer, 'strict')
 
-keyword = "property management software"
+keyword = "insurance leads"
 simple_query = "https://www.google.com/search?num=100&ion=1&espv=&ie=UTF-8&q="
 opener = urllib.request.FancyURLopener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0')]
@@ -47,24 +48,33 @@ def query_url(url):
   url = "http://" + url.replace("http://",'').replace("https://",'') # sanitizing the URI
   #print(url)
   request = opener.open(url)
-  results = request.read()
+  try:
+    results = request.read()
+  except ValueError as e:
+   print('Error:', e)
+   results = None
+   pass
   return results
 
+def remove_html_tags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
 
 def find_2_words_phrases(pagecontents):
-  x = pagecontents.decode('UTF-8').replace('\r','').replace('\n','').replace('\t','').lower().split(' ')
+  x = remove_html_tags(pagecontents.decode('UTF-8').replace('\r','').replace('\n','').replace('\t','').lower()).split(' ')
   new_list = list(filter(None, x))                                                                       # removing empty elements from the list
   two_words_list = list(zip(new_list, new_list[1:]))                                                     # combining consecutive elements together
   return two_words_list
 
-
 def find_3_words_phrases(pagecontents):
-  x = pagecontents.decode('UTF-8').replace('\r','').replace('\n','').replace('\t','').lower().split(' ')
-  new_list = list(filter(None, x))                                                                       # removing empty elements from the list
-  three_words_list = list(zip(new_list, new_list[1:], new_list[2:]))                                     # combining consecutive elements together
+  x = remove_html_tags(pagecontents.decode('UTF-8').replace('\r','').replace('\n','').replace('\t','').lower()).split(' ')
+  new_list = list(filter(None, x))
+  three_words_list = list(zip(new_list, new_list[1:], new_list[2:]))
+  
+  print(three_words_list[2])
   return three_words_list
 
-def leaders(list_of_keyword_phrases, top=1000):    # counting and sorting keywords in the dictionary, from the most often used, to the least often used.
+def leaders(list_of_keyword_phrases, top=100):    # counting and sorting keywords in the dictionary, from the most often used, to the least often used.
     counts = defaultdict(int)                      # top parameter is used to specify how many top keyword phrases to include in the final list
     for keyword_phrase in list_of_keyword_phrases: # Source: http://stackoverflow.com/questions/2290962/python-how-to-get-sorted-count-of-items-in-a-list
         counts[keyword_phrase] += 1
@@ -100,6 +110,9 @@ except http.client.BadStatusLine as e:
 except ValueError as e:
    print('Error:', e)
    pass
+except AttributeError:
+   print("AttributeError found, skipping")
+   pass
 
 for elem in new_links:                          # iterating through the list of Top 10 web pages ranking for the keyword
   z = re.sub("<.*?>", "", elem)                 # removing html tags from the list of website URLs
@@ -116,5 +129,11 @@ graph_3_word = [item for sublist in super_graph_three for item in sublist]
 
 
 zzz = leaders(graph_3_word)
+fh = open("example.txt", "w")
+
 for elem in zzz:
-  print(elem)
+  a = ' '.join(elem[0])
+  print(a, elem[1])
+  fh.write(a + "\t" + str(elem[1]) + "\n")
+
+fh.close()
